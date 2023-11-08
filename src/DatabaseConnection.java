bdd44f66facf1b8209b40df063aa88c1a7aadcab
SultokTheFe <sultok.003@gmail.com>
2023-11-08 15:04:48 +0100

    Implemented CLI interface for the app and implemented 2 new functions (InsertTeam, InsertPlayer)

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,28 +5,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@SuppressWarnings("ClassEscapesDefinedScope")
 public class DatabaseConnection {
     private static DatabaseConnection instance;
     private Connection conn;
 
     private void createTablesIfNotExist() {
         try (Statement stmt = conn.createStatement()) {
-            // Create teams table if not exists
-            String createTeamsTableSQL = "CREATE TABLE IF NOT EXISTS teams (" +
-                    "id SERIAL PRIMARY KEY," +
-                    "team_name VARCHAR(255) UNIQUE NOT NULL" +
+            String createTeamsTableSQL =
+                    "CREATE TABLE IF NOT EXISTS teams (" +
+                        "id SERIAL PRIMARY KEY," +
+                        "team_name VARCHAR(255) UNIQUE NOT NULL" +
                     ")";
             stmt.executeUpdate(createTeamsTableSQL);
 
-            // Create players table if not exists
-            String createPlayersTableSQL = "CREATE TABLE IF NOT EXISTS players (" +
-                    "id SERIAL PRIMARY KEY," +
-                    "name VARCHAR(255) NOT NULL," +
-                    "age INT NOT NULL," +
-                    "position VARCHAR(255) NOT NULL," +
-                    "points INT NOT NULL," +
-                    "team_name VARCHAR(255)," +
-                    "FOREIGN KEY (team_name) REFERENCES teams(team_name)" +
+            String createPlayersTableSQL =
+                    "CREATE TABLE IF NOT EXISTS players (" +
+                        "id SERIAL PRIMARY KEY," +
+                        "name VARCHAR(255) NOT NULL," +
+                        "age INT NOT NULL," +
+                        "position VARCHAR(255) NOT NULL," +
+                        "points INT NOT NULL," +
                     ")";
             stmt.executeUpdate(createPlayersTableSQL);
 
@@ -34,7 +33,6 @@ public class DatabaseConnection {
             e.printStackTrace();
         }
     }
-
 
 
     private DatabaseConnection() {
@@ -55,40 +53,6 @@ public class DatabaseConnection {
             instance = new DatabaseConnection();
         }
         return instance;
-    }
-
-    public Connection getConnection() {
-        return conn;
-    }
-
-    public List<Player> getPlayersFromDatabase() {
-        List<Player> players = new ArrayList<>();
-
-        try (Statement stmt = conn.createStatement()) {
-            String selectPlayersSQL = "SELECT name, age, position, points FROM players";
-            ResultSet rs = stmt.executeQuery(selectPlayersSQL);
-
-            while (rs.next()) {
-                String playerName = rs.getString("name");
-                int playerAge = rs.getInt("age");
-                String playerPosition = rs.getString("position");
-                int playerPoints = rs.getInt("points");
-                Player player = new Player(playerName, playerAge, playerPosition, playerPoints);
-                players.add(player);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return players;
-    }
-    public void addPlayerToDatabase(Player player) {
-        try (Statement stmt = conn.createStatement()) {
-            String insertPlayerSQL = "INSERT INTO players (name, age, position, points) " +
-                    "VALUES ('" + player.getName() + "', " + player.getAge() + ", '" + player.getPosition() + "', " + player.getPoints() + ")";
-            stmt.executeUpdate(insertPlayerSQL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
     public List<Player> getAllPlayersFromDatabase() {
         List<Player> players = new ArrayList<>();
@@ -111,19 +75,6 @@ public class DatabaseConnection {
 
         return players;
     }
-    public void addTeamToDatabase(Team team) {
-        try {
-
-            String insertTeamSQL = "INSERT INTO teams (team_name) VALUES (?)";
-
-            PreparedStatement pstmt = conn.prepareStatement(insertTeamSQL);
-            pstmt.setString(1, team.getName());
-
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public List<Team> getAllTeamsFromDatabase() {
         List<Team> teams = new ArrayList<>();
@@ -142,26 +93,25 @@ public class DatabaseConnection {
         }
         return teams;
     }
-    public List<Player> getPlayersByTeamFromDatabase(String teamName) {
-        List<Player> players = new ArrayList<>();
-        try {
-            String query = "SELECT * FROM players WHERE team_name = ?";
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setString(1, teamName);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                Player player = new Player(
-                        resultSet.getString("name"),
-                        resultSet.getInt("age"),
-                        resultSet.getString("position"),
-                        resultSet.getInt("points")
-                );
-                players.add(player);
-            }
+
+    public void insertTeam(String teamName) {
+        try (Statement stmt = conn.createStatement()) {
+            String insertTeamSQL = "INSERT INTO teams (team_name) VALUES ('" + teamName + "')";
+            stmt.executeUpdate(insertTeamSQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return players;
     }
+
+    public void insertPlayer(String playerName, int age, String position, int points, String teamName) {
+        try (Statement stmt = conn.createStatement()) {
+            String insertPlayerSQL = "INSERT INTO players (name, age, position, points, team_name) " +
+                    "VALUES ('" + playerName + "', " + age + ", '" + position + "', " + points + ", '" + teamName + "')";
+            stmt.executeUpdate(insertPlayerSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
--- src/Hoopify.java
+++ src/Hoopify.java
@@ -1,70 +1,108 @@
-import strategyPattern.StatisticsCalculator;
-import strategyPattern.StatisticsStrategy;
-import strategyPattern.PointsStrategy;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import java.util.Scanner;
 
 public class Hoopify {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+
     public static void main(String[] args) {
-        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+        while (true) {
+            printMenu();
+            int choice = getUserChoice();
 
-        try (Connection conn = dbConnection.getConnection();
-             Statement stmt = conn.createStatement()) {
-
-            PlayerFactory playerFactory = new PlayerFactory();
-            TeamFactory teamFactory = new TeamFactory();
-
-//            Player player1 = playerFactory.createPlayer("Jayson Tatum", 27, "Guard");
-//            Player player2 = playerFactory.createPlayer("Jalen Brown", 23, "Center");
-//            dbConnection.addPlayerToDatabase(player1);
-
-            List<Team> allTeams = dbConnection.getAllTeamsFromDatabase();
-
-            for (Team team : allTeams) {
-                System.out.println("Team Name: " + team.getName());
-
-                // Получите список игроков для текущей команды
-                List<Player> playersInTeam = dbConnection.getPlayersByTeamFromDatabase(team.getName());
-
-                for (Player player : playersInTeam) {
-                    System.out.println("Player Name: " + player.getName());
-                    System.out.println("Age: " + player.getAge());
-                    System.out.println("Position: " + player.getPosition());
-                    System.out.println("Points: " + player.getPoints());
-                    System.out.println("---------------");
-                }
-
-                System.out.println("---------------");
+            switch (choice) {
+                case 1:
+                    showAllTeams();
+                    break;
+                case 2:
+                    showAllPlayers();
+                    break;
+                case 3:
+                    addNewTeam();
+                    break;
+                case 4:
+                    addNewPlayer();
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
-
-            StatisticsStrategy strategy = new PointsStrategy();
-            StatisticsCalculator calculator = new StatisticsCalculator(strategy);
-
-            List<Map<String, Integer>> playerStatsList = new ArrayList<>();
-
-            Map<String, Integer> playerStats1 = new HashMap<>();
-            playerStats1.put("points", 50);
-
-            Map<String, Integer> playerStats2 = new HashMap<>();
-            playerStats2.put("points", 40);
-
-            playerStatsList.add(playerStats1);
-            playerStatsList.add(playerStats2);
-
-            for (Map<String, Integer> playerStats : playerStatsList) {
-                System.out.println("Player Stats: " + playerStats);
-                int playerStatsResult = calculator.calculateStatistic(playerStats);
-                System.out.println("Stats: " + playerStatsResult);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
+
+    private static void printMenu() {
+        System.out.println("Choose an option:");
+        System.out.println("1. See all the teams");
+        System.out.println("2. See all the players");
+        System.out.println("3. Add new team");
+        System.out.println("4. Add new player");
+        System.out.println("5. Quit");
+    }
+
+    private static int getUserChoice() {
+        System.out.print("Enter your choice (1-5): ");
+        try {
+            return scanner.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            // Consume the invalid input
+            scanner.nextLine();
+            return -1; // Return an invalid choice
+        }
+    }
+
+    private static void showAllTeams() {
+        System.out.println("All Teams:");
+        List<Team> allTeams = dbConnection.getAllTeamsFromDatabase();
+        for (Team team : allTeams) {
+            System.out.println("Team Name: " + team.getName());
+        }
+        System.out.println("---------------");
+    }
+
+    private static void showAllPlayers() {
+        System.out.println("All Players:");
+        List<Player> allPlayers = dbConnection.getAllPlayersFromDatabase();
+        for (Player player : allPlayers) {
+            System.out.println("Player Name: " + player.getName());
+            System.out.println("Age: " + player.getAge());
+            System.out.println("Position: " + player.getPosition());
+            System.out.println("Points: " + player.getPoints());
+            System.out.println("Points: " + player.getPoints());
+            System.out.println("---------------");
+        }
+        System.out.println("---------------");
+    }
+
+    private static void addNewTeam() {
+        System.out.print("Enter the name of the new team: ");
+        String teamName = scanner.next();
+        dbConnection.insertTeam(teamName);
+        System.out.println("Team added successfully!");
+        System.out.println("---------------");
+    }
+
+    private static void addNewPlayer() {
+        System.out.println("Enter the details of the new player:");
+        System.out.print("Name: ");
+        String playerName = scanner.next();
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        System.out.print("Position: ");
+        String position = scanner.next();
+        System.out.print("Points: ");
+        int points = scanner.nextInt();
+
+        // Assuming all players will be associated with a team
+        System.out.print("Team Name: ");
+        String teamName = scanner.next();
+        dbConnection.insertPlayer(playerName, age, position, points, teamName);
+        System.out.println("Player added successfully!");
+        System.out.println("---------------");
+    }
 }
+
